253519367712ccb3cec0060975f411e3190d8731
tototriou <tototriou15@gmail.com>
2023-07-28 23:30:44 +0200

    animation of card + social media

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-const cards = document.querySelector(".flip-card");
+const cardStudy = document.querySelector(".studies");
+const cardStudyFlip = document.querySelector(".studiesCard");
+
+const cardHobbies = document.querySelector(".hobbies");
+const cardHobbiesFlip = document.querySelector(".hobbiesCard");
+
+const cardLocation = document.querySelector(".location");
+const cardLocationFlip = document.querySelector(".locationCard");
+
+
+cardStudy.addEventListener("click", () => {
+    cardStudyFlip.classList.toggle("cardActive");
+    cardStudyFlip.classList.toggle("cardUnactive");
+});
+
+cardHobbies.addEventListener("click", () => {
+    cardHobbiesFlip.classList.toggle("cardActive");
+    cardHobbiesFlip.classList.toggle("cardUnactive");
+});
+
+cardLocation.addEventListener("click", () => {
+    cardLocationFlip.classList.toggle("cardActive");
+    cardLocationFlip.classList.toggle("cardUnactive");
+});
