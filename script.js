c5f7941b95fdbbff415ff3e68fcd36be5766da08
Thomas Triouleyre <tototriou15@gmail.com>
2023-08-02 16:08:22 +0200

    about us section

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 const cardStudy = document.querySelector(".studies");
 const cardStudySection = document.querySelector(".studies .aboutSection");
 const cardStudyContent = document.querySelector(".studies .aboutContent");
+const studyBtn = document.querySelector(".studies .btnOpen");
+const studyBtnClose = document.querySelector(".studies .btnClose");
 
-
-const cardHobbies = document.querySelector(".hobbies");
-const cardHobbiesFlip = document.querySelector(".hobbiesCard");
-
-const cardLocation = document.querySelector(".location");
-const cardLocationFlip = document.querySelector(".locationCard");
-
-
-cardStudy.addEventListener("click", () => {
+studyBtn.addEventListener("click", () => {
     cardStudySection.classList.toggle("revealContent");
     cardStudy.classList.toggle("scaleUp");
     cardStudy.classList.toggle("scaleDown");
@@ -19,12 +13,66 @@ cardStudy.addEventListener("click", () => {
     cardStudySection.classList.toggle("unRevealContent");
 });
 
-cardHobbies.addEventListener("click", () => {
-    cardHobbiesFlip.classList.toggle("cardActive");
-    cardHobbiesFlip.classList.toggle("cardUnactive");
+studyBtnClose.addEventListener("click", () => {
+    cardStudySection.classList.toggle("revealContent");
+    cardStudy.classList.toggle("scaleUp");
+    cardStudy.classList.toggle("scaleDown");
+    cardStudyContent.classList.toggle("revealContent");
+    cardStudyContent.classList.toggle("unRevealContent");
+    cardStudySection.classList.toggle("unRevealContent");
 });
 
-cardLocation.addEventListener("click", () => {
-    cardLocationFlip.classList.toggle("cardActive");
-    cardLocationFlip.classList.toggle("cardUnactive");
+
+
+const cardHobby = document.querySelector(".hobbies");
+const cardHobbySection = document.querySelector(".hobbies .aboutSection");
+const cardHobbyContent = document.querySelector(".hobbies .aboutContent");
+const hobbyBtn = document.querySelector(".hobbies .btnOpen");
+const hobbyBtnClose = document.querySelector(".hobbies .btnClose");
+
+hobbyBtn.addEventListener("click", () => {
+    cardHobbySection.classList.toggle("revealContent");
+    cardHobby.classList.toggle("scaleUp");
+    cardHobby.classList.toggle("scaleDown");
+    cardHobbyContent.classList.toggle("revealContent");
+    cardHobbyContent.classList.toggle("unRevealContent");
+    cardHobbySection.classList.toggle("unRevealContent");
 });
+hobbyBtnClose.addEventListener("click", () => {
+    cardHobbySection.classList.toggle("revealContent");
+    cardHobby.classList.toggle("scaleUp");
+    cardHobby.classList.toggle("scaleDown");
+    cardHobbyContent.classList.toggle("revealContent");
+    cardHobbyContent.classList.toggle("unRevealContent");
+    cardHobbySection.classList.toggle("unRevealContent");
+});
+
+
+const cardLocation = document.querySelector(".location");
+const cardLocationSection = document.querySelector(".location .aboutSection");
+const cardLocationContent = document.querySelector(".location .aboutContent");
+const locationBtn = document.querySelector(".location .btnOpen");
+const locationBtnClose = document.querySelector(".location .btnClose");
+
+
+
+
+locationBtn.addEventListener("click", () => {
+    cardLocationSection.classList.toggle("revealContent");
+    cardLocation.classList.toggle("scaleUp");
+    cardLocation.classList.toggle("scaleDown");
+    cardLocationContent.classList.toggle("revealContent");
+    cardLocationContent.classList.toggle("unRevealContent");
+    cardLocationSection.classList.toggle("unRevealContent");
+});
+
+
+locationBtnClose.addEventListener("click", () => {
+    cardLocationSection.classList.toggle("revealContent");
+    cardLocation.classList.toggle("scaleUp");
+    cardLocation.classList.toggle("scaleDown");
+    cardLocationContent.classList.toggle("revealContent");
+    cardLocationContent.classList.toggle("unRevealContent");
+    cardLocationSection.classList.toggle("unRevealContent");
+});
+
