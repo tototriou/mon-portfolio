8d57f94b4e3ab226df8af62691cf93b48fcb005d
Thomas <tototriou15@gmail.com>
2024-06-02 12:53:54 +0200

    add menu button

--- script2.js
+++ script2.js
@@ -30,15 +30,49 @@
 // }
 
 window.addEventListener("scroll", function () {
-    let header = document.querySelector(".home ");
+    let header = document.querySelector(".navbar ");
+    let menu = document.getElementById("menuInside")
     let scrollTop = window.scrollY || document.documentElement.scrollTop;
     console.log(scrollTop)
     header.style.opacity = 1 - (scrollTop - 100) / 500
+    menu.style.opacity = 0 + (scrollTop - 100) / 500
     header.style.transition = "opacity 0.5s"
     if (scrollTop > 100) {
         header.style.zIndex = -1
+        menu.style.zIndex = 1000
+
     } else {
         header.style.zIndex = 1000
+        menu.style.zIndex = -1
+
     }
 
+});
+
+document.getElementById("menu").addEventListener("change", function () {
+    if (this.checked) {
+        let header = document.querySelector(".navbar ");
+        header.style.opacity = 1
+        header.style.transition = "opacity 1s"
+        header.style.zIndex = 1000
+    } else {
+        let header = document.querySelector(".navbar ");
+        header.style.opacity = 0
+        header.style.transition = "opacity 1s"
+        header.style.zIndex = -1
+    }
+});
+
+
+document.getElementById("nav1").addEventListener("click", function () {
+    document.getElementById("menu").checked = false;
+});
+document.getElementById("nav2").addEventListener("click", function () {
+    document.getElementById("menu").checked = false;
+});
+document.getElementById("nav3").addEventListener("click", function () {
+    document.getElementById("menu").checked = false;
+});
+document.getElementById("nav4").addEventListener("click", function () {
+    document.getElementById("menu").checked = false;
 });
