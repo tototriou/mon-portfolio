a2bcc9ffd9db9736364d65f0ba87b08bb6ef9029
Thomas Triouleyre <thomas.triouleyre@kuhn.com>
2024-06-03 16:51:45 +0200

    fix menu + about section

--- script2.js
+++ script2.js
@@ -1,68 +1,18 @@
-// window.onscroll = function () { myFunction() };
-
-
-
-
-// function myFunction() {
-//     const nav1 = document.querySelector("#nav1")
-//     const navbar = document.querySelector(".navbar")
-
-//     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-//         navbar.classList.remove("fadeIn")
-//         navbar.classList.add("fadeOut")
-
-//     } else {
-//         navbar.classList.remove("fadeOut")
-//         navbar.classList.add("fadeIn")
-
-//     }
-// };
-
-// function setHide() {
-//     document.documentElement.style.setProperty("--pseudo-class-nav", 'hidden');
-//     console.log('ok')
-//     // ^^^^^^^^^^^-- select the root 
-// }
-
-// function setVisible() {
-//     document.documentElement.style.setProperty("--pseudo-class-nav", 'visible');
-//     // ^^^^^^^^^^^-- select the root 
-// }
 
 window.addEventListener("scroll", function () {
     let header = document.querySelector(".navbar ");
     let header2 = document.querySelector(".wrapper-navbar ");
     let menu = document.getElementById("menuInside")
     let scrollTop = window.scrollY || document.documentElement.scrollTop;
-    menu.style.opacity = 0 + (scrollTop - 100) / 500
-    header.style.transition = "opacity 0.5s"
-    if (scrollTop > 100) {
-        header.style.zIndex = -1
-        menu.style.zIndex = 1100
-        if (scrollTop > this.window.screen.height / 2) {
-            header2.style.position = "fixed"
-            header2.style.opacity = 0
-            header2.style.transition = "opacity 0s"
-        } else {
-            header2.style.position = "none"
-        }
-
-    } else {
-        header.style.opacity = 1
-        header.style.zIndex = 1000
-        header2.style.opacity = 1
-        header.style.zIndex = 2000
-        header2.style.position = "none"
-
-    }
-
-
+    menu.style.opacity = 0 + (scrollTop - 150) / 500
 
 });
 
 document.getElementById("menu").addEventListener("change", function () {
-    let header = document.querySelector(".wrapper-navbar ");
-    let header2 = document.querySelector(".navbar ");
+    let header = document.querySelector(".wrapper-navbar-fixed");
+    let navbar1 = document.querySelector(".navbar");
+    let header2 = document.querySelector(" .wrapper-navbar-fixed .navbar ");
+    console.log('check')
 
     if (this.checked) {
         header.style.opacity = 1
@@ -71,6 +21,9 @@ document.getElementById("menu").addEventListener("change", function () {
         header2.style.opacity = 1
         header2.style.transition = "opacity 1s"
         header2.style.zIndex = 1000
+        navbar1.style.zIndex = -1
+        navbar1.style.opacity = 0
+        navbar1.style.transition = "opacity 1s"
     } else {
         header.style.opacity = 0
         header.style.transition = "opacity 1s"
@@ -78,19 +31,55 @@ document.getElementById("menu").addEventListener("change", function () {
         header2.style.opacity = 0
         header2.style.transition = "opacity 1s"
         header2.style.zIndex = -1
+        navbar1.style.zIndex = 110
+        navbar1.style.opacity = 1
+        navbar1.style.transition = "opacity 1s"
     }
 });
 
+function handleCheckMenu() {
+    let header = document.querySelector(".wrapper-navbar-fixed");
+    let navbar1 = document.querySelector(".navbar");
+    let header2 = document.querySelector(" .wrapper-navbar-fixed .navbar ");
+    console.log('check')
 
-document.getElementById("nav1").addEventListener("click", function () {
+    if (this.checked) {
+        header.style.opacity = 1
+        header.style.transition = "opacity 1s"
+        header.style.zIndex = 1000
+        header2.style.opacity = 1
+        header2.style.transition = "opacity 1s"
+        header2.style.zIndex = 1000
+        navbar1.style.zIndex = -1
+        navbar1.style.opacity = 0
+        navbar1.style.transition = "opacity 1s"
+    } else {
+        header.style.opacity = 0
+        header.style.transition = "opacity 1s"
+        header.style.zIndex = -1
+        header2.style.opacity = 0
+        header2.style.transition = "opacity 1s"
+        header2.style.zIndex = -1
+        navbar1.style.zIndex = 110
+        navbar1.style.opacity = 1
+        navbar1.style.transition = "opacity 1s"
+    }
+};
+
+
+document.getElementById("nav11").addEventListener("click", function () {
     document.getElementById("menu").checked = false;
+    handleCheckMenu()
 });
-document.getElementById("nav2").addEventListener("click", function () {
+document.getElementById("nav12").addEventListener("click", function () {
     document.getElementById("menu").checked = false;
+    handleCheckMenu()
 });
-document.getElementById("nav3").addEventListener("click", function () {
+document.getElementById("nav13").addEventListener("click", function () {
     document.getElementById("menu").checked = false;
+    handleCheckMenu()
 });
-document.getElementById("nav4").addEventListener("click", function () {
+document.getElementById("nav14").addEventListener("click", function () {
     document.getElementById("menu").checked = false;
+    handleCheckMenu()
 });
