794c6abcb4313279a4327a2b89c8845556e3e288
Thomas Triouleyre <thomas.triouleyre@kuhn.com>
2024-06-06 15:50:21 +0200

    add experiences

--- script2.js
+++ script2.js
@@ -1,39 +1,8 @@
 
-window.addEventListener("scroll", function () {
-    let menu = document.getElementById("menuInside")
-    let scrollTop = window.scrollY || document.documentElement.scrollTop;
-    menu.style.opacity = 0 + (scrollTop - 150) / 500
+// Variables 
 
-});
+// Functions and methods
 
-document.getElementById("menu").addEventListener("change", function () {
-    let header = document.querySelector(".wrapper-navbar-fixed");
-    let navbar1 = document.querySelector(".navbar");
-    let header2 = document.querySelector(" .wrapper-navbar-fixed .navbar ");
-    console.log('check')
-
-    if (this.checked) {
-        header.style.opacity = 1
-        header.style.transition = "opacity 1s"
-        header.style.zIndex = 1000
-        header2.style.opacity = 1
-        header2.style.transition = "opacity 1s"
-        header2.style.zIndex = 1000
-        navbar1.style.zIndex = -1
-        navbar1.style.opacity = 0
-        navbar1.style.transition = "opacity 1s"
-    } else {
-        header.style.opacity = 0
-        header.style.transition = "opacity 1s"
-        header.style.zIndex = -1
-        header2.style.opacity = 0
-        header2.style.transition = "opacity 1s"
-        header2.style.zIndex = -1
-        navbar1.style.zIndex = 110
-        navbar1.style.opacity = 1
-        navbar1.style.transition = "opacity 1s"
-    }
-});
 
 function handleCheckMenu() {
     let header = document.querySelector(".wrapper-navbar-fixed");
@@ -64,6 +33,17 @@ function handleCheckMenu() {
     }
 };
 
+
+// event Listener
+
+window.addEventListener("scroll", function () {
+    let menu = document.getElementById("menuInside")
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+    menu.style.opacity = 0 + (scrollTop - 150) / 500
+
+});
+
+document.getElementById("menu").addEventListener("change", handleCheckMenu);
 
 document.getElementById("nav11").addEventListener("click", function () {
     document.getElementById("menu").checked = false;
